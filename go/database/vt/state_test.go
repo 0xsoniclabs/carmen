54e354779a68d71866b1d6587f645debf421e743
Kamil Jezek <kamil.jezek@gmail.com>
2025-08-02 17:04:18 +0200

    improves tets

--- go/database/vt/state_test.go
+++ go/database/vt/state_test.go
@@ -14,7 +14,6 @@ import (
 	"github.com/0xsoniclabs/carmen/go/database/vt/memory"
 	"github.com/0xsoniclabs/carmen/go/state"
 	"github.com/stretchr/testify/require"
-	"golang.org/x/exp/maps"
 	"io"
 	"testing"
 )
@@ -57,90 +56,7 @@ func TestState_Insert_Single_Values_One_Account_One_Storage(t *testing.T) {
 	require.Equal(t, value, slot, "storage value mismatch for account %x and key %x", addr, key)
 }
 
-func TestState_Insert_Single_Values_Repeat_Various_Updates(t *testing.T) {
-	state, err := initTestedStates(t)
-	require.NoError(t, err, "failed to initialize tested states")
-	defer func() {
-		require.NoError(t, state.Close(), "failed to close state")
-	}()
-
-	addr := common.Address{1}
-	key := common.Key{1}
-
-	// keep last values
-	var lastNonce common.Nonce
-	var lastBalance amount.Amount
-	var lastValue common.Value
-	var lastCode []byte
-	var lastCodeLen int
-	var lastCodeHash common.Hash
-
-	ops := map[string]func(update *common.Update, counter int){
-		"nonce": func(update *common.Update, counter int) {
-			lastNonce = common.ToNonce(uint64(counter))
-			update.Nonces = append(update.Nonces, common.NonceUpdate{Account: addr, Nonce: lastNonce})
-		},
-		"balance": func(update *common.Update, counter int) {
-			lastBalance = amount.New(uint64(counter))
-			update.Balances = append(update.Balances, common.BalanceUpdate{Account: addr, Balance: lastBalance})
-		},
-		"storage": func(update *common.Update, counter int) {
-			lastValue = common.Value{byte(counter), byte(counter >> 8)}
-			update.Slots = append(update.Slots, common.SlotUpdate{Account: addr, Key: key, Value: lastValue})
-		},
-		"create": func(update *common.Update, counter int) {
-			update.CreatedAccounts = append(update.CreatedAccounts, addr)
-		},
-		"code": func(update *common.Update, counter int) {
-			lastCode = make([]byte, 1024+counter)
-			for i := 0; i < len(lastCode); i++ {
-				lastCode[i] = byte(i + counter)
-			}
-			lastCodeLen = len(lastCode)
-			lastCodeHash = common.Keccak256(lastCode)
-			update.Codes = append(update.Codes, common.CodeUpdate{Account: addr, Code: lastCode})
-		},
-	}
-
-	const numOps = 1000
-	keys := maps.Keys(ops)
-	for i := 0; i < numOps; i++ {
-		update := common.Update{}
-
-		// select an operation to perform
-		opName := keys[i%len(keys)]
-		ops[opName](&update, i)
-
-		require.NoError(t, state.Apply(uint64(i), update), "failed to apply update for operation %s at iteration %d", opName, i)
-
-		// check hash consistency
-		hash, err := state.GetHash()
-		require.NoError(t, err, "failed to get hash for operation %s at iteration %d", opName, i)
-		require.NotEmpty(t, hash, "hash should not be empty for operation %s at iteration %d", opName, i)
-
-		nonce, err := state.GetNonce(addr)
-		require.NoError(t, err, "failed to get nonce for account %x", addr)
-		require.Equal(t, common.ToNonce(1), nonce, "nonce mismatch for account %x", addr)
-
-		balance, err := state.GetBalance(addr)
-		require.NoError(t, err, "failed to get balance for account %x", addr)
-		require.Equal(t, amount.New(1), balance, "balance mismatch for account %x", addr)
-
-		value, err := state.GetStorage(addr, key)
-		require.NoError(t, err, "failed to get storage for account %x and key %x", addr, key)
-		require.Equal(t, lastValue, value, "storage value mismatch for account %x and key %x", addr, key)
-
-		codeLen, err := state.GetCodeSize(addr)
-		require.NoError(t, err, "failed to get code size for account %x", addr)
-		require.Equal(t, lastCodeLen, codeLen, "code size mismatch for account %x", addr)
-
-		codeHash, err := state.GetCodeHash(addr)
-		require.NoError(t, err, "failed to get code hash for account %x", addr)
-		require.Equal(t, lastCodeHash, codeHash, "code hash mismatch for account %x", addr)
-	}
-}
-
-func TestState_CreateAccounts_In_Blocks_Properties_Available(t *testing.T) {
+func TestState_CreateAccounts_In_Blocks_Accounts_Updated(t *testing.T) {
 	state, err := initTestedStates(t)
 	require.NoError(t, err, "failed to initialize tested states")
 	defer func() {
@@ -194,6 +110,7 @@ func TestState_Storage_Can_Set_And_Receive(t *testing.T) {
 		for j := 0; j < numKeysPerAddress; j++ {
 			key := common.Key{byte(i), byte(i >> 8), byte(j), byte(j >> 8)}
 			value := common.Value{byte(i), byte(i >> 8), byte(j), byte(j >> 8)}
+			update.Codes = append(update.Codes, common.CodeUpdate{Account: addr, Code: []byte{}})
 			update.Slots = append(update.Slots, common.SlotUpdate{Account: addr, Key: key, Value: value})
 		}
 	}
