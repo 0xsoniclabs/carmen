5ff0407a25c02110e36de25b80fa31e2842c823d
ihtefyw <ph3work@proton.me>
2025-08-20 11:38:47 +0200

    Add Cache interface

---
+++ rust/src/cache/mod.rs
@@ -0,0 +1,39 @@
+use crate::error::Error;
+
+/// An abstraction representing a cache.
+#[allow(dead_code)]
+pub trait Cache {
+    /// The type of the ID used to identify [`Self::Item`] in the cache.
+    type Id;
+    /// The type of the payload stored in the cache.
+    type ItemPayload;
+    /// The type stored in the cache.
+    type StoredItem;
+
+    /// Retrieves an entry from the cache.
+    fn get(&self, id: Self::Id) -> Result<Self::StoredItem, Error>;
+
+    /// Stores the value in the cache and reserves an ID for it.
+    fn set(&self, value: Self::ItemPayload) -> Result<Self::Id, Error>;
+
+    /// Deletes the entry with the given ID from the cache and storage.
+    /// The ID may be reused in the future.
+    fn delete(
+        &self,
+        id: Self::Id,
+        #[cfg(test)] _test_notify: Option<std::sync::mpsc::Sender<DeleteStatusMsg>>,
+    ) -> Result<(), Error>;
+
+    /// Flushes all cache elements
+    fn flush(&self) -> Result<(), Error>;
+}
+
+/// A utility enum to notify tests about the status of a cache deletion operation.
+/// This is useful to check if the cache is waiting for entry references to be released before
+/// deletion
+#[allow(dead_code)]
+#[cfg(test)]
+pub enum DeleteStatusMsg {
+    Waiting,
+    CacheDeleted,
+}
