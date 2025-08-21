02a19194b4d524304d2f8c805e61dd95b52338ca
ihtefyw <ph3work@proton.me>
2025-08-21 13:17:58 +0200

    Rename Id to Key

--- rust/src/cache/mod.rs
+++ rust/src/cache/mod.rs
@@ -4,23 +4,23 @@ use crate::error::Error;
 #[allow(dead_code)]
 pub trait Cache {
     /// The type of the ID used to identify [`Self::Item`] in the cache.
-    type Id;
+    type Key;
     /// The type of the payload stored in the cache.
     type ItemPayload;
     /// The type returned by the cache.
     type Item;
 
     /// Retrieves an entry from the cache.
-    fn get(&self, id: Self::Id) -> Result<Self::Item, Error>;
+    fn get(&self, id: Self::Key) -> Result<Self::Item, Error>;
 
     /// Stores the value in the cache and reserves an ID for it.
-    fn set(&self, value: Self::ItemPayload) -> Result<Self::Id, Error>;
+    fn set(&self, value: Self::ItemPayload) -> Result<Self::Key, Error>;
 
     /// Deletes the entry with the given ID from the cache and storage.
     /// The ID may be reused in the future.
     fn delete(
         &self,
-        id: Self::Id,
+        id: Self::Key,
         #[cfg(test)] _test_notify: Option<std::sync::mpsc::Sender<DeleteStatusMsg>>,
     ) -> Result<(), Error>;
 
