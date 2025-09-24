048aca429c5bb4c3deb5e8d0cabd0ee405a1977d
ihtefyw <ph3work@proton.me>
2025-09-24 11:58:05 +0200

    Fix naming

--- rust/src/node_manager/mod.rs
+++ rust/src/node_manager/mod.rs
@@ -20,39 +20,39 @@ use crate::error::Error;
 /// Nodes are uniquely identified by a [`NodeManager::Id`] and are owned by the node manager.
 /// They can be accessed through read or write locks with the [`NodeManager::get_read_access`] and
 /// [`NodeManager::get_write_access`] methods.
-/// IDs are managed by the node manager itself, which hands out new IDs upon insertion of an item.
+/// IDs are managed by the node manager itself, which hands out new IDs upon insertion of a node.
 /// IDs are not globally unique and may be reused after deletion.
 ///
 /// The concrete type returned by the [`NodeManager`] may not be [`NodeManager::NodeType`] but
 /// instead a wrapper type which dereferences to [`NodeManager::NodeType`]. This abstraction allows
-/// for the node manager to associate metadata with each item, for example to implement smart cache
+/// for the node manager to associate metadata with each node, for example to implement smart cache
 /// eviction.
 #[allow(dead_code)]
 pub trait NodeManager {
-    /// The id type used to identify items in the node manager.
+    /// The ID type used to identify nodes in the node manager.
     type Id;
     /// The node type indexed by the node manager.
     type NodeType;
 
-    /// Adds the item in the node manager and returns an ID for it.
+    /// Adds the given node to the node manager and returns an ID for it.
     fn add(&self, item: Self::NodeType) -> Result<Self::Id, Error>;
 
-    /// Returns a read guard for an item in the node manager, if it exists. Returns
+    /// Returns a read guard for a node in the node manager, if it exists. Returns
     /// [`crate::storage::Error::NotFound`] otherwise.
     fn get_read_access(
         &self,
         id: Self::Id,
     ) -> Result<RwLockReadGuard<'_, impl Deref<Target = Self::NodeType>>, Error>;
 
-    /// Returns a write guard for an item in the node manager, if it exists. Returns
+    /// Returns a write guard for a node in the node manager, if it exists. Returns
     /// [`crate::storage::Error::NotFound`] otherwise.
     fn get_write_access(
         &self,
         id: Self::Id,
     ) -> Result<RwLockWriteGuard<'_, impl DerefMut<Target = Self::NodeType>>, Error>;
 
-    /// Deletes an item with the given ID from the node manager.
-    /// The ID may be reused in the future, when creating a new item by calling
+    /// Deletes a node with the given ID from the node manager.
+    /// The ID may be reused in the future, when adding new nodes using
     /// [`NodeManager::add`].
     fn delete(&self, id: Self::Id) -> Result<(), Error>;
 
