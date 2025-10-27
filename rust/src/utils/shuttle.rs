4e6272262e459006f01ac9b4c1af103cfdb2e2e3
ihtefyw <ph3work@proton.me>
2025-10-27 16:22:43 +0100

    Add licence

--- rust/src/utils/shuttle.rs
+++ rust/src/utils/shuttle.rs
@@ -1,3 +1,13 @@
+// Copyright (c) 2025 Sonic Operations Ltd
+//
+// Use of this software is governed by the Business Source License included
+// in the LICENSE file and at soniclabs.com/bsl11.
+//
+// Change Date: 2028-4-16
+//
+// On the date above, in accordance with the Business Source License, use of
+// this software will be governed by the GNU Lesser General Public License v3.
+
 /// Helper function to run a test with `shuttle` using the random scheduler for `_max_iter`
 /// iterations.
 /// It uses two environment variables to control its behavior:
