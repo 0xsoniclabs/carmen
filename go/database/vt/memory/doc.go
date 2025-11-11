8fd0d13829eb30e66b1fee0a7d536bcf6ed82111
Herbert Jordan <herbert@swrnd.ch>
2025-11-11 14:53:29 +0100

    Reuse code chunking code

--- go/database/vt/memory/doc.go
+++ go/database/vt/memory/doc.go
@@ -8,19 +8,19 @@
 // On the date above, in accordance with the Business Source License, use of
 // this software will be governed by the GNU Lesser General Public License v3.
 
-// Package memory implements a simple reference Verkle trie (S6) for the Carmen
+// Package memory implements a simple in-memory Verkle trie (S6) for the Carmen
 // project providing
-//   - a proof-of-concept implementation of the Verkle trie in Carmen
-//   - an executable specification for developers
-//   - a light-weight reference implementation for testing of other components
+//   - a performance-oriented implementation of the Verkle trie in Carmen
+//   - a proofing ground for optimizations on the Go side of Carmen
+//   - a benchmark implementation for comparing with other implementations
 //
-// The implementation is maintained to be suitable for testing purposes for all
-// potential use cases, with the following limitations:
+// The implementation is maintained to be suitable for testing and benchmarking
+// purposes for all potential use cases, with the following limitations:
 //   - it retains all data in memory, so it can not scale to large datasets
 //   - there is no support for checkpointing to recover from crashes
 //
-// WARNING: This package is not intended for production use. It is a reference
-// implementation and is not optimized for performance or memory usage. It is
-// also lacking sufficient testing and error handling. It is only intended
-// to be used as a reference for the Verkle trie implementation in Carmen.
+// WARNING: This package is not intended for production use. It is a prototype
+// implementation and is not optimized for memory usage. It is also lacking
+// sufficient testing and error handling. It is only intended to be used as a
+// prototype for Verkle trie implementations in Carmen.
 package memory
