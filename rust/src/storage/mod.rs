178557710057be407b50abfb2994bec40a6a381c
ihtefyw <ph3work@proton.me>
2025-11-12 11:05:18 +0100

    Add caching benchmark

--- rust/src/storage/mod.rs
+++ rust/src/storage/mod.rs
@@ -113,3 +113,35 @@ pub trait RootIdProvider {
     /// numbers previously passed to `set_root_id`.
     fn set_root_id(&self, block_number: u64, id: Self::Id) -> BTResult<(), Error>;
 }
+
+impl<S> Storage for Box<S>
+where
+    S: Storage + ?Sized + 'static,
+{
+    type Id = S::Id;
+    type Item = S::Item;
+
+    fn open(_path: &Path) -> BTResult<Self, Error> {
+        unimplemented!("cannot open storage from a boxed trait object")
+    }
+
+    fn get(&self, id: Self::Id) -> BTResult<Self::Item, Error> {
+        self.as_ref().get(id)
+    }
+
+    fn reserve(&self, item: &Self::Item) -> Self::Id {
+        self.as_ref().reserve(item)
+    }
+
+    fn set(&self, id: Self::Id, item: &Self::Item) -> BTResult<(), Error> {
+        self.as_ref().set(id, item)
+    }
+
+    fn delete(&self, id: Self::Id) -> BTResult<(), Error> {
+        self.as_ref().delete(id)
+    }
+
+    fn close(self) -> BTResult<(), Error> {
+        unimplemented!("cannot call close from a boxed trait object");
+    }
+}
