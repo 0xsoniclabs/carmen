323a750df50c7320bc4b6722602836bcc3b803c9
Herbert Jordan <herbert@swrnd.ch>
2026-02-18 17:47:42 +0100

    Rebuild state to use new store infrastructure

--- go/database/vt/geth2/state.go
+++ go/database/vt/geth2/state.go
@@ -1,33 +1,53 @@
 package geth2
 
 import (
+	"context"
+	"encoding/binary"
 	"errors"
 	"fmt"
-	"os"
+	"io"
 	"path/filepath"
 
 	"github.com/0xsoniclabs/carmen/go/common"
+	"github.com/0xsoniclabs/carmen/go/common/amount"
+	"github.com/0xsoniclabs/carmen/go/common/future"
+	"github.com/0xsoniclabs/carmen/go/common/result"
+	"github.com/0xsoniclabs/carmen/go/common/witness"
 	"github.com/0xsoniclabs/carmen/go/state"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/trie/utils"
 	"github.com/ethereum/go-verkle"
+	"github.com/holiman/uint256"
 )
 
 type verkleState struct {
-	root verkle.VerkleNode
-
-	directory string
-	store     NodeStore
-
-	pathBased bool
+	root   verkle.VerkleNode
+	store  Store
+	source NodeSource
 }
 
 func newState(
 	params state.Parameters,
 ) (_ *verkleState, err error) {
 	dataDir := params.Directory
-	pathBased := params.Archive == state.NoArchive
+
+	var liveOnly bool
+	switch params.Archive {
+	case state.NoArchive:
+		liveOnly = true
+	case state.LevelDbArchive:
+		liveOnly = false
+	default:
+		return nil, fmt.Errorf("unsupported archive mode: %v", params.Archive)
+	}
 
 	// Open the node store (e.g., LevelDB) for the given directory.
-	store, err := newLevelDbStore(filepath.Join(dataDir, "nodes"))
+	var store Store
+	if liveOnly {
+		store, err = newLevelDbLiveStore(filepath.Join(dataDir, "nodes"))
+	} else {
+		store, err = newLevelDbArchiveStore(filepath.Join(dataDir, "nodes"))
+	}
 	if err != nil {
 		return nil, fmt.Errorf("failed to open node store: %w", err)
 	}
@@ -38,23 +58,20 @@ func newState(
 		}
 	}()
 
-	// Load the key for the root node.
-	rootNodeKey := []byte{} // empty for path-based storage
-	if !pathBased {
-		// If not path-based, the root file contains the root node key.
-		rootFile := getRootFilePath(dataDir)
-		if _, err := os.Stat(rootFile); err == nil {
-			rootId, err := os.ReadFile(rootFile)
-			if err != nil || len(rootId) != 32 {
-				return nil, fmt.Errorf("failed to read root file: %w", err)
-			}
-			rootNodeKey = rootId
-		}
+	res, err := _newStateWithSource(store.HeadState())
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize state: %w", err)
 	}
+	res.store = store
+	success = true
+	return res, nil
+}
 
-	// Load the root node.
+func _newStateWithSource(source NodeSource) (_ *verkleState, err error) {
+
+	// Load the root node, if present.
 	var root verkle.VerkleNode
-	if rootData, err := store.Get(rootNodeKey); err == ErrNotFound {
+	if rootData, err := source.GetNode(nil); err == ErrNotFound {
 		root = verkle.New()
 	} else if err != nil {
 		return nil, fmt.Errorf("failed to read root node from store: %w", err)
@@ -66,26 +83,25 @@ func newState(
 		root = rootNode
 	}
 
-	success = true
 	return &verkleState{
-		root:      root,
-		directory: dataDir,
-		store:     store,
-		pathBased: pathBased,
+		root:   root,
+		source: source,
 	}, nil
 }
 
-func (s *verkleState) SetNonce(
-	address common.Address,
-	nonce common.Nonce,
-) error {
-	key := common.Keccak256ForAddress(address)
-	return s.root.Insert(key[:], nonce[:], s.readNode)
+// --- State interface implementation ---
+
+func (s *verkleState) Exists(address common.Address) (bool, error) {
+	panic("not implemented")
 }
 
-func (s *verkleState) GetNonce(
-	address common.Address,
-) (common.Nonce, error) {
+func (s *verkleState) GetBalance(address common.Address) (amount.Amount, error) {
+	panic("not implemented")
+}
+
+func (s *verkleState) GetNonce(address common.Address) (common.Nonce, error) {
+
+	// TODO: replace with proper tree embedding
 	key := common.Keccak256ForAddress(address)
 	fmt.Printf("Getting nonce for address %v with key %x\n", address, key)
 	data, err := s.root.Get(key[:], s.readNode)
@@ -95,63 +111,216 @@ func (s *verkleState) GetNonce(
 	var nonce common.Nonce
 	copy(nonce[:], data)
 	return nonce, nil
+
+}
+
+func (s *verkleState) GetStorage(address common.Address, key common.Key) (common.Value, error) {
+	panic("not implemented")
+}
+
+func (s *verkleState) GetCode(address common.Address) ([]byte, error) {
+	panic("not implemented")
+}
+
+func (s *verkleState) GetCodeSize(address common.Address) (int, error) {
+	panic("not implemented")
+}
+
+func (s *verkleState) GetCodeHash(address common.Address) (common.Hash, error) {
+	panic("not implemented")
+}
+
+func (s *verkleState) HasEmptyStorage(addr common.Address) (bool, error) {
+	return true, nil
+}
+
+func (s *verkleState) Apply(block uint64, update common.Update) error {
+
+	// ------------------------------------------------------------------
+	// 				  Implement all state modifications
+	// ------------------------------------------------------------------
+
+	// TODO: do proper updates, including embedding.
+	for _, update := range update.Nonces {
+		key := common.Keccak256ForAddress(update.Account)
+		s.root.Insert(key[:], update.Nonce[:], s.readNode)
+	}
+
+	// ------------------------------------------------------------------
+
+	// Compute and commit all changes to the store.
+	nodes, err := s.root.(*verkle.InternalNode).BatchSerialize()
+	if err != nil {
+		return fmt.Errorf("failed to serialize nodes: %w", err)
+	}
+	changes := make([]Entry, 0, len(nodes))
+	for _, node := range nodes {
+		changes = append(changes, Entry{
+			Path: node.Path,
+			Blob: node.SerializedBytes,
+		})
+	}
+
+	// Limit memory usage by pruning in-memory tree structure at a certain depth.
+	// The nodes will be reloaded from the store when needed.
+	s.root.(*verkle.InternalNode).FlushAtDepth(3, func([]byte, verkle.VerkleNode) {
+		// no-op callback, since all nodes are stored as deltas.
+	})
+
+	return s.store.AddBlock(block, changes)
+}
+
+func (s *verkleState) GetHash() (common.Hash, error) {
+	return s.GetCommitment().Await().Get()
+}
+
+func (s *verkleState) GetCommitment() future.Future[result.Result[common.Hash]] {
+	return future.Immediate(result.Ok(common.Hash(s.root.Commitment().Bytes())))
 }
 
 func (s *verkleState) Flush() error {
-	var issues []error
-	flush := func(key []byte, node verkle.VerkleNode) {
-		if err := s.flushNode(key, node); err != nil {
-			issues = append(issues, err)
-		}
+	if s.store == nil {
+		return nil
 	}
-
-	// Flush all nodes.
-	switch node := s.root.(type) {
-	case *verkle.InternalNode:
-		node.Flush(flush)
-		// TODO: use FlushAtDepth to manage memory usage and performance trade-offs
-		// node.FlushAtDepth(3, flush)
-	}
-
-	// Write root key to file.
-	rootId := s.root.Commitment().Bytes()
-	rootFilePath := getRootFilePath(s.directory)
-	if err := os.WriteFile(rootFilePath, rootId[:], 0644); err != nil {
-		issues = append(issues, fmt.Errorf("failed to write root file: %w", err))
-	}
-
-	return errors.Join(issues...)
+	return s.store.Flush()
 }
 
 func (s *verkleState) Close() error {
-	return errors.Join(s.Flush(), s.store.Close())
+	if s.store == nil {
+		return nil
+	}
+	err := s.store.Close()
+	s.store = nil
+	return err
 }
+
+func (s *verkleState) GetMemoryFootprint() *common.MemoryFootprint {
+	panic("not implemented")
+}
+
+func (s *verkleState) GetArchiveState(block uint64) (state.State, error) {
+	source, err := s.store.HistoricState(block)
+	if err != nil {
+		return nil, err
+	}
+	// For simplicity, we are re-using the verkleState implementation for the
+	// historic state. The store field remains nil set in this state view.
+	return _newStateWithSource(source)
+}
+
+func (s *verkleState) GetArchiveBlockHeight() (height uint64, empty bool, err error) {
+	next := s.store.NextBlock()
+	if next == 0 {
+		return 0, true, nil
+	}
+	return next - 1, false, nil
+}
+
+func (s *verkleState) Check() error {
+	panic("not implemented")
+}
+
+func (s *verkleState) CreateWitnessProof(address common.Address, keys ...common.Key) (witness.Proof, error) {
+	panic("not implemented")
+}
+
+func (s *verkleState) Export(ctx context.Context, out io.Writer) (common.Hash, error) {
+	panic("not implemented")
+}
+
+// --- utility functions ---
 
 func (s *verkleState) readNode(path []byte) ([]byte, error) {
 	fmt.Printf("Reading node %x\n", path)
-	return s.store.Get(path)
+	if s.source == nil {
+		// In live mode, the source is nil, and we read directly from the
+		// store's head state.
+		return s.store.HeadState().GetNode(path)
+	}
+	// In archive mode, the source is initialized with a view on the node store
+	// at the requested block height.
+	return s.source.GetNode(path)
 }
 
-func (s *verkleState) flushNode(path []byte, node verkle.VerkleNode) error {
-	data, err := node.Serialize()
+func (s *verkleState) getAccountOrEmptyState(address common.Address) (*types.StateAccount, error) {
+	account, err := s.getAccount(address)
 	if err != nil {
-		return fmt.Errorf("node serialization failed: %w", err)
+		return nil, err
 	}
-	hash := node.Commitment().Bytes()
-
-	// TODO: support live and archive modes by optionally only writing the node
-	// to the path and not the hash.
-
-	fmt.Printf("Flushing node %x / %x\n", path, hash)
-	if s.pathBased {
-		return s.store.Set(path, data)
+	if account == nil {
+		account = types.NewEmptyStateAccount()
 	}
-	return errors.Join(
-		s.store.Set(path, data),
-		s.store.Set(hash[:], data),
-	)
+	return account, nil
 }
 
-func getRootFilePath(dir string) string {
-	return filepath.Join(dir, "root")
+// --- Ported from go-Ethereum, as this code got removed from the main branch --
+
+var (
+	errInvalidRootType = errors.New("invalid node type for root")
+)
+
+func (s *verkleState) getAccount(address common.Address) (*types.StateAccount, error) {
+	// TODO: reference source of this logic;
+
+	var (
+		acc    = &types.StateAccount{}
+		values [][]byte
+		err    error
+	)
+	switch n := s.root.(type) {
+	case *verkle.InternalNode:
+		values, err = n.GetValuesAtStem(s.cache.GetStem(address[:]), s.readNode)
+		if err != nil {
+			return nil, fmt.Errorf("GetAccount (%x) error: %v", address, err)
+		}
+	default:
+		return nil, errInvalidRootType
+	}
+	if values == nil {
+		return nil, nil
+	}
+	basicData := values[utils.BasicDataLeafKey]
+	acc.Nonce = binary.BigEndian.Uint64(basicData[utils.BasicDataNonceOffset:])
+	acc.Balance = new(uint256.Int).SetBytes(basicData[utils.BasicDataBalanceOffset : utils.BasicDataBalanceOffset+16])
+	acc.CodeHash = values[utils.CodeHashLeafKey]
+
+	// TODO account.Root is leave as empty. How should we handle the legacy account?
+	return acc, nil
+}
+
+// UpdateAccount implements state.Trie, writing the provided account into the tree.
+// If the tree is corrupted, an error will be returned.
+func (s *verkleState) UpdateAccount(addr common.Address, acc *types.StateAccount, codeLen int) error {
+	// TODO: reference source of this logic;
+
+	var (
+		err       error
+		basicData [32]byte
+		values    = make([][]byte, verkle.NodeWidth)
+		stem      = s.cache.GetStem(addr[:])
+	)
+
+	// Code size is encoded in BasicData as a 3-byte big-endian integer. Spare bytes are present
+	// before the code size to support bigger integers in the future. PutUint32(...) requires
+	// 4 bytes, so we need to shift the offset 1 byte to the left.
+	binary.BigEndian.PutUint32(basicData[utils.BasicDataCodeSizeOffset-1:], uint32(codeLen))
+	binary.BigEndian.PutUint64(basicData[utils.BasicDataNonceOffset:], acc.Nonce)
+	if acc.Balance.ByteLen() > 16 {
+		panic("balance too large")
+	}
+	acc.Balance.WriteToSlice(basicData[utils.BasicDataBalanceOffset : utils.BasicDataBalanceOffset+16])
+	values[utils.BasicDataLeafKey] = basicData[:]
+	values[utils.CodeHashLeafKey] = acc.CodeHash[:]
+
+	switch root := s.root.(type) {
+	case *verkle.InternalNode:
+		err = root.InsertValuesAtStem(stem, values, s.readNode)
+	default:
+		return errInvalidRootType
+	}
+	if err != nil {
+		return fmt.Errorf("UpdateAccount (%x) error: %v", addr, err)
+	}
+
+	return nil
 }
--- go/database/vt/geth2/store.go
+++ go/database/vt/geth2/store.go
@@ -1,74 +1,256 @@
 package geth2
 
 import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"fmt"
+
 	"github.com/0xsoniclabs/carmen/go/common"
+	"github.com/0xsoniclabs/carmen/go/state"
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/opt"
 )
 
 const (
-	ErrNotFound = common.ConstError("not found")
+	ErrNotFound  = common.ConstError("not found")
+	ErrNoArchive = state.NoArchiveError
 )
 
-// NodeStore is an interface for a key-value store used to persist Verkle nodes.
-type NodeStore interface {
-	Get(key []byte) ([]byte, error)
-	Set(key []byte, value []byte) error
+// Store is an interface for storing and retrieving Verkle Trie nodes, as well
+// as keeping track of the current block number. It serves as the interface to
+// the underlying storage layer (e.g., LevelDB) for the Verkle Trie state.
+type Store interface {
+	// NextBlock returns the block number of the next block to be added to the store.
+	NextBlock() uint64
+	// HeadState returns a NodeSource for the head state of the store (i.e., the
+	// state at the latest block).
+	HeadState() NodeSource
+
+	// AddBlock adds a new block with the given block number and changes to the
+	// store. The changes should cover the delta between the previous head state
+	// and the new head state introduced by this block.
+	AddBlock(block uint64, changes []Entry) error
+
+	// HistoricState returns a NodeSource for the state at the given block number.
+	// If the store does not support historical states, it returns ErrNoArchive.
+	HistoricState(block uint64) (NodeSource, error)
+
+	// Flush flushes any pending changes to the underlying storage.
+	Flush() error
+
+	// Close closes the store and releases any resources it holds.
 	Close() error
 }
 
-// levelDbStore is a simple implementation of NodeStore using LevelDB.
-type levelDbStore struct {
-	db *leveldb.DB
+type NodeSource interface {
+	GetNode(path []byte) ([]byte, error)
 }
 
-func newLevelDbStore(path string) (*levelDbStore, error) {
+type Entry struct {
+	Path []byte
+	Blob []byte
+}
+
+// --- Factories ---
+
+// newLevelDbLiveStore creates a LevelDB-backed store that only keeps the latest
+// state (head) in the DB. It does not support historical states, and returns
+// ErrNoArchive if a historic state is requested.
+func newLevelDbLiveStore(path string) (*levelDbStore, error) {
+	// This implementation uses the path of a node as a key in the DB directly.
+	// When nodes are updated, old versions of the node are overwritten.
+	return newLevelDbStore(
+		path,
+		func(_ uint64, path []byte) []byte {
+			return path
+		},
+		func(db *leveldb.DB, block uint64, path []byte) ([]byte, error) {
+			data, err := db.Get(path, &opt.ReadOptions{})
+			if err == leveldb.ErrNotFound {
+				return nil, ErrNotFound
+			}
+			return data, err
+		},
+		false,
+	)
+}
+
+// newLevelDbArchiveStore creates a LevelDB-backed store that keeps all
+// historical states in the DB.
+func newLevelDbArchiveStore(path string) (*levelDbStore, error) {
+	// This implementation uses a composite key of the path and the block number
+	// for each node. When nodes are updated, new entries are added to the DB with
+	// the same path but different block numbers. When a historic state is requested,
+	// the store looks for the entry with the largest block number that is smaller
+	// or equal to the requested block number.
+	return newLevelDbStore(
+		path,
+		toArchivePath,
+		func(db *leveldb.DB, block uint64, path []byte) ([]byte, error) {
+			// In the archive, we need to find the last block that updated this
+			// path before or at the given block, and read the node from that
+			// block.
+			key := toArchivePath(block, path)
+			iter := db.NewIterator(nil, &opt.ReadOptions{})
+			defer iter.Release()
+			if iter.Seek(key) {
+				if bytes.Equal(iter.Key(), key) {
+					// There is a perfect match for this block and path, return it.
+					return iter.Value(), nil
+				}
+			}
+			// No perfect match, we need to find the last update for this path
+			// before this block.
+			if iter.Prev() {
+				// Check if the previous entry is for the same path.
+				prevKey := iter.Key()
+				if areArchiveKeysForSamePath(prevKey, key) {
+					return iter.Value(), nil
+				}
+			}
+			return nil, ErrNotFound
+		},
+		true,
+	)
+}
+
+// --- Implementations ---
+
+type KeyFactoryFn func(block uint64, path []byte) []byte
+type NodeFinderFn func(db *leveldb.DB, block uint64, path []byte) ([]byte, error)
+
+// levelDbStore is a generic store implementation instantiated by factory
+// functions to realize different storage strategies (live vs archive).
+type levelDbStore struct {
+	db              *leveldb.DB
+	nextBlock       uint64
+	keyFactory      KeyFactoryFn
+	findNode        NodeFinderFn
+	supportsHistory bool
+}
+
+func newLevelDbStore(
+	path string,
+	keyFactory KeyFactoryFn,
+	findNode NodeFinderFn,
+	supportsHistory bool,
+) (*levelDbStore, error) {
 	db, err := leveldb.OpenFile(path, nil)
 	if err != nil {
 		return nil, err
 	}
-	return &levelDbStore{db: db}, nil
-}
 
-func (s *levelDbStore) Get(key []byte) ([]byte, error) {
-	data, err := s.db.Get(key, &opt.ReadOptions{})
-	if err == leveldb.ErrNotFound {
-		return nil, ErrNotFound
+	// Load the next block from the DB, if it exists.
+	nextBlock, err := loadNextBlockFromDb(db)
+	if err != nil {
+		return nil, errors.Join(err, db.Close())
 	}
-	return data, err
+
+	return &levelDbStore{
+		db:              db,
+		nextBlock:       nextBlock,
+		keyFactory:      keyFactory,
+		findNode:        findNode,
+		supportsHistory: supportsHistory,
+	}, nil
 }
 
-func (s *levelDbStore) Set(key []byte, value []byte) error {
-	return s.db.Put(key, value, &opt.WriteOptions{})
+func (s *levelDbStore) NextBlock() uint64 {
+	return s.nextBlock
+}
+
+func (s *levelDbStore) HeadState() NodeSource {
+	return &levelDbNodeSource{db: s.db, block: s.nextBlock, find: s.findNode}
+}
+
+func (s *levelDbStore) AddBlock(block uint64, changes []Entry) error {
+	for _, change := range changes {
+		key := s.keyFactory(block, change.Path)
+		fmt.Printf("Storing node %x at block %d\n", change.Path, block)
+		if err := s.db.Put(key, change.Blob, &opt.WriteOptions{}); err != nil {
+			return err
+		}
+	}
+	s.nextBlock = block + 1
+	return storeNextBlockToDb(s.db, block+1)
+}
+
+func (s *levelDbStore) HistoricState(block uint64) (NodeSource, error) {
+	if !s.supportsHistory {
+		return nil, ErrNoArchive
+	}
+	if block >= s.nextBlock {
+		return nil, fmt.Errorf("requested block %d is in the future (next block: %d)", block, s.nextBlock)
+	}
+	return &levelDbNodeSource{db: s.db, block: block, find: s.findNode}, nil
+}
+
+func (s *levelDbStore) Flush() error {
+	// no-op, as flushes are managed by LevelDB internally.
+	return nil
 }
 
 func (s *levelDbStore) Close() error {
 	return s.db.Close()
 }
 
-// memoryDbStore is a simple in-memory implementation of NodeStore for testing purposes.
-type memoryDbStore struct {
-	store map[string][]byte
+type levelDbNodeSource struct {
+	db    *leveldb.DB
+	block uint64
+	find  NodeFinderFn
 }
 
-func newMemoryDbStore() *memoryDbStore {
-	return &memoryDbStore{store: make(map[string][]byte)}
+func (s *levelDbNodeSource) GetNode(path []byte) ([]byte, error) {
+	fmt.Printf("Getting node %x at block %d\n", path, s.block)
+	return s.find(s.db, s.block, path)
 }
 
-func (s *memoryDbStore) Get(key []byte) ([]byte, error) {
-	value, ok := s.store[string(key)]
-	if !ok {
-		return nil, ErrNotFound
+func toArchivePath(block uint64, path []byte) []byte {
+	res := make([]byte, 1+len(path)+4)
+	res[0] = byte(len(path))
+	copy(res[1:], path)
+	binary.BigEndian.PutUint32(res[1+len(path):], uint32(block))
+	return res
+}
+
+func areArchiveKeysForSamePath(key1, key2 []byte) bool {
+	if len(key1) != len(key2) {
+		return false
 	}
-	return value, nil
+	if len(key1) < 4 {
+		return false
+	}
+	// Mask out the last 4 byte (block number) and compare the rest of the key,
+	// which corresponds to the path.
+	key1 = key1[:len(key1)-4]
+	key2 = key2[:len(key2)-4]
+	return bytes.Equal(key1, key2)
 }
 
-func (s *memoryDbStore) Set(key []byte, value []byte) error {
-	s.store[string(key)] = value
-	return nil
+// --- Utilities ---
+
+var (
+	nextBlockKey = []byte{63: 0} // longer than any other key, to avoid collisions
+)
+
+func loadNextBlockFromDb(db *leveldb.DB) (uint64, error) {
+	key := nextBlockKey
+	if data, err := db.Get(key, &opt.ReadOptions{}); err == nil {
+		if len(data) != 8 {
+			return 0, fmt.Errorf("invalid next block data")
+		}
+		nextBlock := binary.BigEndian.Uint64(data)
+		return nextBlock, nil
+	} else if err != leveldb.ErrNotFound {
+		return 0, err
+	}
+	return 0, nil // default to 0 if not found
 }
 
-func (s *memoryDbStore) Close() error {
-	// No resources to clean up for in-memory store.
-	return nil
+func storeNextBlockToDb(db *leveldb.DB, nextBlock uint64) error {
+	key := nextBlockKey
+	data := make([]byte, 8)
+	binary.BigEndian.PutUint64(data, nextBlock)
+	return db.Put(key, data, &opt.WriteOptions{})
 }
