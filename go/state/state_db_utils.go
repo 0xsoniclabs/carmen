d1898de2fecc69d55be5bb479eaf3ac845d1f8e3
ihtefyw <ph3work@proton.me>
2026-03-13 10:46:02 +0100

    Add combinatorial testing to transaction revert

---
+++ go/state/state_db_utils.go
@@ -0,0 +1,60 @@
+package state
+
+import "slices"
+
+// cloneMapWith clones `m` using `cloneFunc` to clone the values. If `m` is nil, it returns nil.
+func cloneMapWith[K comparable, V any](m map[K]V, cloneFunc func(V) V) map[K]V {
+	if m == nil {
+		return nil
+	}
+	cloned := make(map[K]V, len(m))
+	for k, v := range m {
+		cloned[k] = cloneFunc(v)
+	}
+	return cloned
+}
+
+func cloneBalanceValue(bv *balanceValue) *balanceValue {
+	if bv == nil {
+		return nil
+	}
+	cloned := *bv
+	if bv.original != nil {
+		originalCopy := *bv.original
+		cloned.original = &originalCopy
+	}
+	return &cloned
+}
+
+func cloneNonceValue(nv *nonceValue) *nonceValue {
+	if nv == nil {
+		return nil
+	}
+	cloned := *nv
+	if nv.original != nil {
+		originalCopy := *nv.original
+		cloned.original = &originalCopy
+	}
+	return &cloned
+}
+
+func cloneCodeValue(cv *codeValue) *codeValue {
+	if cv == nil {
+		return nil
+	}
+	cloned := *cv
+	if cv.hash != nil {
+		hashCopy := *cv.hash
+		cloned.hash = &hashCopy
+	}
+	cloned.code = slices.Clone(cv.code)
+	return &cloned
+}
+
+func cloneValue[V any](v *V) *V {
+	if v == nil {
+		return nil
+	}
+	cloned := *v
+	return &cloned
+}
